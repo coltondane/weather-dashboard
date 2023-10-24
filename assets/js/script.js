679c1a06f885c0aa23c8339ad883c196f302a0b8
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-24 03:19:52 -0600

    working on accessing input value, is blank

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 const apiKey = '457a3c3118899b78bfcb79e03960442b'
 
 // document variables
-const searchBar = document.querySelector('#city-input');
+const searchBarVal = document.querySelector('input').value;
 const searchBtn = document.querySelector('#search-btn');
 const clearSearchBtn = document.querySelector('#clear-history');
+
+// functions
+
+ function getCity(cityName) {
+    // if there is a city value
+    return function () {
+        console.log(cityName);
+    }
+ };
+
+//  event listeners
+ searchBtn.addEventListener('click', getCity(searchBarVal));
