0d58bd155800665faeefaffd5f91c77ab98a4357
coltondane <113216904+coltondane@users.noreply.github.com>
2023-10-24 02:46:36 -0600

    adding variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 // API key
 const apiKey = '457a3c3118899b78bfcb79e03960442b'
 
+// document variables
+const searchBar = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search-btn');
+const clearSearchBtn = document.querySelector('#clear-history');
