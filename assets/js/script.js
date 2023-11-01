6df107ba75c4b761033b948f6abb4f2538b52c8a
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-01 01:45:34 -0600

    stored all initial values into variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,82 @@
 // API key
-const apiKey = '457a3c3118899b78bfcb79e03960442b'
+const apiKey = "457a3c3118899b78bfcb79e03960442b";
 
 // load local storage
 const historyStorage = JSON.parse(localStorage.getItem("searchHistory"));
 
 // document variables
-const searchBar = document.querySelector('#search-bar');
-
-const searchBtn = document.querySelector('#search-btn');
-const clearSearchBtn = document.querySelector('#clear-history');
-
-const setLocation = document.querySelector('.weather-location');
+const searchBar = document.querySelector("#search-bar");
+// buttons
+const searchBtn = document.querySelector("#search-btn");
+const clearSearchBtn = document.querySelector("#clear-history");
+// HTML elements
+const setLocation = document.querySelector(".weather-location");
 
 // functions
-
 function getLocation() {
-        // get city name on click
-        if (searchBar.value) {
-            const cityName = searchBar.value;
-            // get latitude and longitude
-            const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
-    
-            fetch(geoURL)
-            .then((response) => {
-                console.log(response.status);
-                // if the response isnt good display it
-                if (response.status !== 200) {
-                    alert(`Must Enter Valid City Name: ${response.status}`)
-    
-                }
-                // else return 
-                return response.json();
-            })
-            .then((data) => {
-                console.log(data);
-                const lon = data[0].lon;
-                const lat = data[0].lat
-                console.log(lat, lon);
-                // after gathering the lat and lon run the nnext function
-                getCityData(lat, lon);
-            })
+  // get city name on click
+  if (searchBar.value) {
+    const cityName = searchBar.value;
+    // get latitude and longitude
+    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+
+    fetch(geoURL)
+      .then((response) => {
+        console.log(response.status);
+        // if the response isnt good display it
+        if (response.status !== 200) {
+          alert(`Must Enter Valid City Name: ${response.status}`);
         }
-};
+        // else return
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const lon = data[0].lon;
+        const lat = data[0].lat;
+        console.log(lat, lon);
+        // after gathering the lat and lon run the nnext function
+        getCityData(lat, lon);
+      });
+  }
+}
 
 function getCityData(lat, lon) {
-    // if there is a city value
-        // get weather data API
-        const apiURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  // if there is a city value
+  // get weather data API
+  const apiURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
-        fetch(apiURL)
-        .then((response) => {
-            console.log(response.status);
-            // if the response isnt good display it
-            if (response.status !== 200) {
-                alert(`Must Enter Valid City Name: ${response.status}`)
-
-            }
-            // else return 
-            return response.json();
-        })
-        .then((data) => {
-            console.log("all data: ", data);
-            console.log("City Data: ", data.city);
-            console.log("Future Data: ", data.list);
-            // current and future conditions
-            // city name, date, weather icon, temp, humidity, wind speed
-            // future 5 day forecast w/ date, weather icon, temp, wind speed, amd humidity
-            displayWeather(data);
-        })
-};
+  fetch(apiURL)
+    .then((response) => {
+      console.log(response.status);
+      // if the response isnt good display it
+      if (response.status !== 200) {
+        alert(`Must Enter Valid City Name: ${response.status}`);
+      }
+      // else return
+      return response.json();
+    })
+    .then((data) => {
+      console.log("all data: ", data);
+      console.log("City Data: ", data.city);
+      console.log("Future Data: ", data.list);
+      // current and future conditions
+      // future 5 day forecast w/ date, weather icon, temp, wind speed, amd humidity
+      displayWeather(data);
+    });
+}
 
 function displayWeather(data) {
-    // store the weather data in variables
-    const selectedLocation = `${data.city.name}, ${data.city.country}`
-    console.log(selectedLocation);
-    const currentWeather = data.list[0].main
-    console.log("weather: ", currentWeather);
-
+  // store the weather data in variables
+  // city name-, date-, weather icon, temp-, humidity-, wind speed-
+  const selectedLocation = `${data.city.name}, ${data.city.country}`;
+  const currentTemp = `${Math.round(data.list[0].main.temp)}°F`;
+  const description = data.list[0].weather[0].description;
+  const currentHumidity = `${data.list[0].main.humidity}%`;
+  const windSpeed = `${data.list[0].wind.speed}mph`;
+  const date = data.list[0].dt_txt
+  console.log(description);
 }
 
 //  event listeners
- searchBtn.addEventListener('click', getLocation);
+searchBtn.addEventListener("click", getLocation);
