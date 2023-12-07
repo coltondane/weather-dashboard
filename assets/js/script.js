2baa520472f4fe2977aeb12fc02375b45e2fe063
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-07 15:18:45 -0700

    search history working

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ const searchBar = document.querySelector("#search-bar");
 const searchBtn = document.querySelector("#search-btn");
 const clearSearchBtn = document.querySelector("#clear-history");
 // HTML elements
-const setHistory = document.querySelector("#history");
+const setHistory = document.querySelector("#history-display");
 const setLocation = document.querySelector(".weather-location");
 const setDate = document.querySelector(".weather-date");
 const setDay = document.querySelector("#day");
@@ -21,18 +21,39 @@ const setWindSpeed = document.querySelector("#wind-speed");
 const setHumidity = document.querySelector("#humidity");
 const forecastContainer = document.querySelector("#forecast-container");
 
-
 // functions
-// function displaySearchHistory {
-//   for (let i = 0; i < storageArray.length; i++) {
-    
-    
-//   }
-// }
+function displaySearchHistory() {
+  console.log("displaySearchHistory");
+  setHistory.innerHTML = "";
+  for (let i = 0; i < storageArray.length; i++) {
+    // create a new li for each item in the array
+    const cityHistory = document.createElement("li");
+    // set the text content to the array item
+    cityHistory.textContent = storageArray[i];
+    cityHistory.className = "history-list";
+    // add an event listener to each item to search that city on click
+    setHistory.addEventListener("click", function (event) {
+      if (event.target.tagName === "LI") {
+        // Check if the clicked element is an li
+        getLocation(event.target.textContent);
+      };
+    });
+    // append
+    setHistory.appendChild(cityHistory);
+  }
+}
+
 function getLocation() {
   // get city name on click
   if (searchBar.value) {
     const cityName = searchBar.value;
+    // add the city to search history if it isn't already there
+    if (!storageArray.includes(cityName)) {
+      // add the city to the search history in local storage
+      storageArray.push(cityName);
+      localStorage.setItem("searchHistory", JSON.stringify(storageArray));
+      displaySearchHistory();
+    }
     // displaySearchHistory(cityName);
     // get latitude and longitude
     const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
@@ -66,7 +87,7 @@ function getCityData(lat, lon) {
   fetch(apiURL)
     .then((response) => {
       console.log(response.status);
-      // if the response isnt good display it
+      // if the response isn't good display it
       if (response.status !== 200) {
         alert(`Must Enter Valid City Name: ${response.status}`);
       }
@@ -85,7 +106,7 @@ function getCityData(lat, lon) {
 
 function displayWeather(data) {
   // clear the forecast container
-  forecastContainer.innerHTML = '';
+  forecastContainer.innerHTML = "";
   // store the weather data in variables
   // city name-, date-, weather icon, temp-, humidity-, wind speed-
   const selectedLocation = `${data.city.name}, ${data.city.country}`;
@@ -105,37 +126,36 @@ function displayWeather(data) {
 
   // gather the noon forecast for each set of data
   const forecast = data.list.filter((list) => {
-    return list.dt_txt.includes('12:00:00');
+    return list.dt_txt.includes("12:00:00");
   });
   console.log(forecast);
 
   // create a new child div for each day
   for (let i = 0; i < forecast.length; i++) {
-    const divContainer = document.createElement('div');
-    divContainer.className = 'weakly-weather-item';
-  
-    const forecastTemp = document.createElement('p');
-    forecastTemp.textContent = `Temp: ${forecast[i].main.temp}°F`;
-  
-    const forecastWind = document.createElement('p');
+    const divContainer = document.createElement("div");
+    divContainer.className = "weakly-weather-item";
+
+    const forecastTemp = document.createElement("p");
+    forecastTemp.textContent = `Temp: ${Math.round(forecast[i].main.temp)}°F`;
+
+    const forecastWind = document.createElement("p");
     forecastWind.textContent = `Wind: ${forecast[i].wind.speed}mph`;
-  
-    var forecastHumidity = document.createElement('p');
+
+    var forecastHumidity = document.createElement("p");
     forecastHumidity.textContent = `Humidity: ${forecast[i].main.humidity}%`;
-  
+
     var iconUrl = `https://openweathermap.org/img/wn/${forecast[i].weather[0].icon}@2x.png`;
-    var icon = document.createElement('img');
+    var icon = document.createElement("img");
     icon.src = iconUrl;
-    
+
     divContainer.appendChild(icon);
     divContainer.appendChild(forecastHumidity);
     divContainer.appendChild(forecastWind);
     divContainer.appendChild(forecastTemp);
-    
+
     // append the weather data for each one
     forecastContainer.appendChild(divContainer);
-  } 
-
+  }
 }
 
 //  event listeners
